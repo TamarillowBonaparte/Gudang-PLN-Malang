6db057fb600bb5c51a97f945900827bb09a0a50f
MOH. DANI KURNIAWAN SUGIARTO <125776654+TamarillowBonaparte@users.noreply.github.com>
2024-08-23 09:59:37 +0700

    data akun

--- resources/views/daftarakun.blade.php
+++ resources/views/daftarakun.blade.php
@@ -20,11 +20,11 @@
     <!-- Vendor CSS Files -->
     <link href="{{asset ('admin/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{asset ('admin/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-    <!-- <link href="{{asset ('admin/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet"> -->
-    <!-- <link href="{{asset ('admin/assets/vendor/quill/quill.snow.css')}}" rel="stylesheet"> -->
-    <!-- <link href="{{asset ('admin/assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet"> -->
-    <!-- <link href="{{asset ('admin/assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet"> -->
-    <!-- <link href="{{asset ('admin/assets/vendor/simple-datatables/style.css')}}" rel="stylesheet"> -->
+    <link href="{{asset ('admin/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{asset ('admin/assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
+    <link href="{{asset ('admin/assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet"> -->
+    <link href="{{asset ('admin/assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet"> -->
+    <link href="{{asset ('admin/assets/vendor/simple-datatables/style.css')}}" rel="stylesheet"> -->
 
     <!-- Template Main CSS File -->
     <link href="{{asset ('admin/assets/css/style.css')}}" rel="stylesheet">
@@ -108,7 +108,7 @@
                         <tbody>
                             <tr>
                                 <td>wisnu</td>
-                                <td>pedo</td>
+                                <td>123</td>
                                 <td>vendor</td>
                                 <td>
                                     <div class="btn"><i class="bi bi-brush"></i></div>
