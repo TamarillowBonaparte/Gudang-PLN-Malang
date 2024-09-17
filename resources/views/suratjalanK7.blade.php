d091d11b0e729b2609feebd8e4d1a99d25a0c0dc
faizbangett <148948420+faizbangett@users.noreply.github.com>
2024-09-17 15:49:13 +0700

    dawd

---
+++ resources/views/suratjalanK7.blade.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Surat Angkutan</title>
+    <link rel="stylesheet" href="style_k7.css" />
+  </head>
+  <body>
+    <div class="kop-surat">
+      <div class="logo-container">
+        <img src="assets/logo PLN.png" class="logo" />
+      </div>
+      <div class="info-container">
+        <p><strong>PT PLN (PERSERO)</strong></p>
+        <p><strong>UNIT INDUK DISTRIBUSI (UID) JAWA TIMUR</strong></p>
+        <p><strong>UNIT PELAKSANA PELAYANAN PELANGGAN (UP3) MALANG</strong></p>
+      </div>
+    </div>
+
+    <div class="surat-angkutan">
+      <div class="title">
+        <h3>SURAT ANGKUTAN</h3>
+        <p id="pe">......./LOG.08.02/GD. ARIES/VI/2024</p>
+      </div>
+
+      <div class="detail-info">
+        
+        <div class="left-detail">
+          <p>Kendaraan No.  &emsp;&ensp;&ensp;&ensp;&nbsp;:  N 8979 BF</p>
+          <p>Nama Pengemudi &emsp;&nbsp;&nbsp;&nbsp;: SULIS</p>
+          <p>Dari Logistik  &emsp;&ensp;&ensp;&ensp;&ensp;&ensp;&nbsp;:  UP3 MALANG</p>
+        </div>
+
+        <div class="right-detail">
+
+          <table>
+            
+          <tr>
+          <td rowspan="5" id="panjangne">
+            KEPADA &nbsp;:
+            <br>
+            <br>
+            SPK NO. &ensp;:&emsp;</td>
+          <td ></td>
+          </tr>
+
+          <tr>
+            <td ></td>
+          </tr>
+
+          <tr>
+            <td id="kiri1">PT. NATA USAHA BERSAMA</td>
+          </tr>
+
+          <tr>
+            <td></td>
+          </tr>
+
+          <tr>
+          <td id="kiri2">8039/PJ/DAN.01.03/F04700080/2024 - PK/REN.UP3-MLG/2024</td>
+          </tr>
+
+          
+          </table>
+
+        </div>
+      </div>
+
+      <table class="barang-table">
+        <thead>
+          <tr>
+            <th>No. Urut</th>
+            <th>Nama Material</th>
+            <th>Normalisasi <br> Material</th>
+            <th>Satuan</th>
+            <th>Jumlah <br> Material</th>
+            <th colspan="2">Keterangan</th>
+          </tr>
+          
+        </thead>
+        <tbody>
+          <tr id="yowes">
+            <td id="nourut"              rowspan="2">1</td>
+            <td id="namamaterial"        rowspan="2">BOX;APPVI TR 53 KVA;AL PLATE 2MM;</td>
+            <td id="normalisasimaterial" rowspan="2"></td>
+            <td id="satuan"              rowspan="2">M</td>
+            <td id="jumlahmaterial"      rowspan="2">1</td>
+            <td id="keterangan1"         >NO RESERVASI</td>
+            <td id="ket1"                rowspan="2">&nbsp;DI</td>
+          </tr>
+
+          <tr id="yowes">
+            <td id="keterangan2">SAP :</td>
+          </tr>
+
+          <tr id="yowes">
+            <td id="nourut"              rowspan="2">1</td>
+            <td id="namamaterial"        rowspan="2">BOX;APPVI TR 53 KVA;AL PLATE 2MM;</td>
+            <td id="normalisasimaterial" rowspan="2"></td>
+            <td id="satuan"              rowspan="2">M</td>
+            <td id="jumlahmaterial"      rowspan="2">1</td>
+            <td id="keterangan1"         >NAMA</td>
+            <td id="ket1"                rowspan="2">&nbsp;DA</td>
+          </tr>
+
+          <tr id="yowes">
+            <td id="keterangan2">PELANGGAN :</td>
+          </tr>
+
+          <tr id="yowes">
+            <td id="nourut"              >1</td>
+            <td id="namamaterial"        >BOX;APPVI TR 53 KVA;AL PLATE 2MM;</td>
+            <td id="normalisasimaterial" ></td>
+            <td id="satuan"              >M</td>
+            <td id="jumlahmaterial"      >1</td>
+            <td id="keterangan3"         >ULP :</td>
+            <td id="ket1"                >&nbsp;DA</td>
+            </tr>
+
+            <tr id="yowes">
+              <td id="nourut"              >1</td>
+              <td id="namamaterial"        >BOX;APPVI TR 53 KVA;AL PLATE 2MM;</td>
+              <td id="normalisasimaterial" ></td>
+              <td id="satuan"              >M</td>
+              <td id="jumlahmaterial"      >1</td>
+              <td id="keterangan3"         >ALAMAT</td>
+              <td id="ket1"                >&nbsp;DA</td>
+              </tr>
+
+              <tr id="yowes">
+                <td id="nourut"              >1</td>
+                <td id="namamaterial"        >BOX;APPVI TR 53 KVA;AL PLATE 2MM;</td>
+                <td id="normalisasimaterial" ></td>
+                <td id="satuan"              >M</td>
+                <td id="jumlahmaterial"      >1</td>
+                <td id="keterangan3"         >PB / PD :</td>
+                <td id="ket1"                >&nbsp;DA</td>
+                </tr>
+
+                <tr id="yowes">
+                  <td id="nourut"              >1</td>
+                  <td id="namamaterial"        >BOX;APPVI TR 53 KVA;AL PLATE 2MM;</td>
+                  <td id="normalisasimaterial" ></td>
+                  <td id="satuan"              >M</td>
+                  <td id="jumlahmaterial"      >1</td>
+                  <td id="keterangan3"         ></td>
+                  <td id="ket1"                ></td>
+                  </tr>
+
+                  <tr id="yowes">
+                    <td id="nourut"              >1</td>
+                    <td id="namamaterial"        >BOX;APPVI TR 53 KVA;AL PLATE 2MM;</td>
+                    <td id="normalisasimaterial" ></td>
+                    <td id="satuan"              >M</td>
+                    <td id="jumlahmaterial"      >1</td>
+                    <td id="keterangan3"         >ID PELANGGAN :</td>
+                    <td id="ket1"                >&nbsp;DA</td>
+                    </tr>
+
+                    <tr id="yowes">
+                      <td id="nourut"                  >1</td>
+                      <td id="namamaterial"            >BOX;APPVI TR 53 KVA;AL PLATE 2MM;</td>
+                      <td id="normalisasimaterial"     ></td>
+                      <td id="satuan"                  >M</td>
+                      <td id="jumlahmaterial"          >1</td>
+                      <td id="keterangan4" colspan="2" >TUG 123989132D</td>
+                      </tr>
+
+            
+
+        </tbody>
+      </table>
+
+      <div class="footer-info">
+       
+        <div class="left-footer">
+          Diterima tgl&emsp;........................
+        </div>
+
+        <div class="right-footer">
+          Malang,&emsp;.............................
+        </div>
+      </div>
+
+        <div class="signature-section">
+
+          <div class="left-signature">
+              <br>
+              <p>Penerima,</p>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <p>SANTI</p>
+          </div>
+
+          <div class="center-signature">
+            <p>Mengetahui,</p>
+            <p>Security,</p>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <p>...................</p>
+        </div>
+      
+          <div class="right-signature">
+              <br>
+              <p>Yang menyerahkan,</p>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <p>LIA KURNIAWATI</p>
+          </div>
+
+        </div>
+      
+
+  </body>
+</html>
