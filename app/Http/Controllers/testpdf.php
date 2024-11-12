34c08633061ae485b845b28ecd41c92b686b5d48
Wisnuuh <136870014+Wisnuuh@users.noreply.github.com.>
2024-11-12 16:01:44 +0700

    fixing page size into landscape, trying to change font

--- app/Http/Controllers/testpdf.php
+++ app/Http/Controllers/testpdf.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Barryvdh\DomPDF\Facade\Pdf;
+use Barryvdh\DomPDF\Facade\Pdf as FacadePdf;
+use Dompdf\Dompdf;
 use Illuminate\Http\Request;
 
 class testpdf extends Controller
 {
     public function index () {
 
-        $pdf = Pdf::loadView('testpdf');
- 
+        $pdf = FacadePdf::loadView('testprint');
+        
+        // Set additional PDF options if needed
+        $pdf->setOptions([
+            'fontDir' => public_path('/fonts'),
+            'fontCache' => public_path('/fonts'),
+            'defaultFont' => 'RobotoMono-Regular'
+        ]);
+
         return $pdf->stream();
-        // return view('testpdf');
+        // return view('testprint');
     }
 }
